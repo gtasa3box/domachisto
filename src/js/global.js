35580fb4e41d7484d98ddb4644fb73a6a969cfa4
gtasa3box <gtasa3box@gmail.com>
2021-11-19 15:43:31 +0300

    commit

--- src/js/global.js
+++ src/js/global.js
@@ -2,3 +2,7 @@
 $('.close').on('click', function(){
     $($(this).data('target')).toggleClass('not-active')  
 });
+//drop-nav
+$('#uslugi').on('click', function(){
+    $($(this).data('target')).toggleClass('active')  
+});
