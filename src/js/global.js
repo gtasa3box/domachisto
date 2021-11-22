eb67bb14b86d0a175a0697d35ac4c7c895185647
gtasa3box <gtasa3box@gmail.com>
2021-11-22 18:15:42 +0300

    first commit

--- src/js/global.js
+++ src/js/global.js
@@ -16,4 +16,45 @@ $('.tabs__nav-elem').on('click', function(){
     $('.tabs__elem').removeClass('active')
     $($(this).data('target')).addClass('active')
 });
-    
+
+//examples slider
+$('.slider-examples').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+});
+
+//number input flag 
+const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      initialCountry: "ru",
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    });
